2f5b60f82ee4164c039d9124e4518c1d2a384997
Folk <Sutthichai.rhe@chonvc.ac.th>
2024-10-25 15:45:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+<span>
+     <h1>1</h1>
+     <h2>2</h2>
+     <h3>3</h3>
+
+</span>
+<span>
+     <h4>4</h4>
+     <h5>5</h5>
+</span>
+    </>
+
+  )
+}
+
+export default App
